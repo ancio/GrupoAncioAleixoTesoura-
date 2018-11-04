5f4c33f5c9840b691f22973fd0aca0f5068b4fb5
Angelo Machirika <Anciomagaio@gmail.com>
2018-11-04 16:45:46 +0200

    classe da tela principal

--- EmpregoSociomatico/app/src/main/java/at/ancio/empregosociomatico/activities/VagasActivity.java
+++ EmpregoSociomatico/app/src/main/java/at/ancio/empregosociomatico/activities/VagasActivity.java
@@ -2,6 +2,7 @@ package at.ancio.empregosociomatico.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class VagasActivity  extends AppCompatActivity{
 
@@ -9,6 +10,23 @@ public class VagasActivity  extends AppCompatActivity{
     protected  void  onCreate(Bundle savedIntanceState){
         super.onCreate(savedIntanceState);
         setContentView(R.layout.activity_vagas);
+
+
+        getSupportActionBar().hide();
+
+
+        String nome = getIntent().getExtras().getString(key: "vagas_name");
+        String Empresa = getIntent().getExtras().getString(key: "vagas_Empresa");
+        String Disponibilidade = getIntent().getExtras().getString(key: "Vagas_Disponibilidade");
+        String Data = getIntent().getExtras().getString(key: "vagas_Data");
+        String Descricao = getIntent().getExtras().getString(key: "Vagas_Descricao");
+        String imagem = getIntent().getExtras().getString(key: "Vagas_imagem");
+
+        CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.collapsingtoolbar_id);
+        collapsingToolbarLayout.setTitleEnabled(true);
+
+        TextView  = findViewById(R.aavaga);
+        TextView t
     }
 
 
--- EmpregoSociomatico/app/src/main/java/at/ancio/empregosociomatico/adapters/RecyclerViewAdapter.java
+++ EmpregoSociomatico/app/src/main/java/at/ancio/empregosociomatico/adapters/RecyclerViewAdapter.java
@@ -1,12 +1,14 @@
 package at.ancio.empregosociomatico.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -15,6 +17,7 @@ import com.bumptech.glide.request.RequestOptions;
 import java.util.List;
 
 import at.ancio.empregosociomatico.R;
+import at.ancio.empregosociomatico.activities.VagasActivity;
 import at.ancio.empregosociomatico.models.Vagas;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
@@ -35,7 +38,25 @@ RequestOptions option;
       View view;
         LayoutInflater inflater = LayoutInflater.from(mContext);
         view = inflater.inflate(R.layout.vagas_row_item,parent,false);
-        return new ViewHolder(view);
+        final ViewHolder viewHolder = new ViewHolder(view);
+           viewHolder.view_container.setOnClickListener(new View.OnClickListener(){
+
+
+               @Override
+               public void onClick(View v) {
+                   Intent i = new Intent(mContext, VagasActivity.class);
+                   i.putExtra( name "Vagas_name",mData.get(viewHolder.getAdapterPosition()).getNome());
+                   i.putExtra( name "Vagas_Empresa",mData.get(viewHolder.getAdapterPosition()).getEmpresa());
+                   i.putExtra( name "Vagas_Disponibilidade",mData.get(viewHolder.getAdapterPosition()).getDisponibilidade());
+                   i.putExtra( name "Vagas_Data",mData.get(viewHolder.getAdapterPosition()).getData());
+                   i.putExtra( name "Vagas_Descricao",mData.get(viewHolder.getAdapterPosition()).getDescription());
+                   i.putExtra( name "Vagas_imagem",mData.get(viewHolder.getAdapterPosition()).getImage());
+
+              mContext.startActivity(i);
+               }
+           });
+
+        return viewHolder;
     }
 
     @Override
@@ -59,10 +80,11 @@ RequestOptions option;
         TextView vagas_empresa;
         TextView vagas_disponibilidade;
         ImageView img_thumbnail;
-
+        LinearLayout view_container;
 
         public ViewHolder(View itemView) {
             super(itemView);
+            view_container = itemView.findViewById(R.id.container);
             vagas_nome= itemView.findViewById(R.id.vaga_nome);
             vagas_empresa = itemView.findViewById(R.id.Empresa);
             vagas_disponibilidade = itemView.findViewById(R.id.Disponibilidade);
